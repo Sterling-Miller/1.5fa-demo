c4c90b27ca5d212994e3afb714401bec3f0a3790
Leo Schonberger (Windows Desktop) <leoschonberger@gmai.com>
2025-03-04 23:16:02 -0800

    Cleaned up qr-component and added nav-bar

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,33 +1,16 @@
 import { auth, signOut } from 'app/auth';
 import QRScanner from 'app/qrReader';
+import Navbar from '../navbar';
 
 export default async function ProtectedPage() {
   let session = await auth();
 
   return (
-    <div className="flex h-screen bg-black">
-      <div className="w-screen h-screen flex flex-col space-y-5 justify-center items-center text-white">
-        You are logged in as {session?.user?.email}
-        <SignOut />
-        <div>
-          <h1>Scan QR Code</h1>
-              <QRScanner/>
-
-        </div>
+    <div className="flex h-screen w-screen bg-slate-900 ">
+      <div className="flex flex-col space-y-5 justify-center items-center text-white w-full h-full">
+        <Navbar currentUser={session?.user?.email} onSignOut={signOut} />
+        <QRScanner/>
       </div>
     </div>
-  );
-}
-
-function SignOut() {
-  return (
-    <form
-      action={async () => {
-        'use server';
-        await signOut();
-      }}
-    >
-      <button type="submit">Sign out</button>
-    </form>
   );
 }
--- app/qrReader.tsx
+++ app/qrReader.tsx
@@ -1,37 +1,38 @@
 'use client'
 import { useState } from "react";
-import QrReader from "modern-react-qr-reader";
+import QrReader from "modern-react-qr-reader"; 
+// Sourse Inspo: https://codesandbox.io/p/sandbox/qrscanner-854b39?file=%2Fsrc%2FApp.js%3A3%2C1-4%2C1
 
-const App = () => {
+const QRScanner = () => {
   const [code, setCode] = useState(null);
   const [showDialog, setShowDialog] = useState(false);
   const [processing, setProcessing] = useState(false);
   const [precScan, setPrecScan] = useState("");
   const [selected, setSelected] = useState("environment");
 
-  const handleScan = (scanData) => {
+  const handleScan = (scanData: string) => { // maybe not string
     if (scanData && scanData) {
       // window.location.href = scanData;
       console.log(scanData); // Here is our scanned data!
     }
   };
   
-  const handleError = (err) => {
+  const handleError = (err: any) => {
     console.error(err);
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-blue-600">
-      <h1 className="text-4xl font-bold text-white mb-8 mt-20">Escaneá el código QR</h1>
-      <p className="text-xl text-gray-300 mb-8">Apúntate con la cámara del código, se detectará automáticamente</p>
+    <div className="flex flex-col items-center justify-center bg-slate-500">
+      <h1 className="text-4xl font-bold text-white mb-8 mt-20">Scan the QR Code</h1>
       
       <div className="w-full max-w-md space-y-4">
         <select 
           onChange={(e) => setSelected(e.target.value)}
-          className="w-full p-2 border border-gray-700 rounded-lg bg-white shadow-sm"
+          className="w-full p-2 border border-slate-700 rounded-lg bg-white shadow-sm"
         >
-          <option value={"environment"} className="text-gray-800">Cámara trasera</option>
-          <option value={"user"} className="text-gray-800">Cámara delantera</option>
+          <option value="" disabled selected className="text-gray-800">Select a camera</option>
+          <option value={"environment"} className="text-gray-800">Rear Camera</option>
+          <option value={"user"} className="text-gray-800">Front Camera</option>
         </select>
       </div>
       
@@ -41,11 +42,11 @@ const App = () => {
           delay={500}
           onScan={handleScan}
           onError={handleError}
-          className="qr-reader w-[80vw] mt-20"
+          className="qr-reader w-[40vw] mt-20 bg-slate-300"
         />
       )}
     </div>
   );
 };
 
-export default App;
+export default QRScanner;
