2be202cd321456a9f72a8deae2e1f99eb53061b2
Sterling-Miller <stermiller11@gmail.com>
2025-03-09 14:43:40 -0700

    commit for vercel

--- app/api/verifytoken/route.ts
+++ app/api/verifytoken/route.ts
@@ -7,6 +7,7 @@ export async function POST(req: Request) {
     const tokenDataArray = await getTokenData(token);
     const tokenData = tokenDataArray[0];
 
+    // TODO: check logic here
     if (!tokenData || tokenData.used || !tokenData.expiresat || tokenData.expiresat < new Date()) {
       return NextResponse.json({ success: false, message: "Invalid or expired token" }, { status: 400 });
     }
