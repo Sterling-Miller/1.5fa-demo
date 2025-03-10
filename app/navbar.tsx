282530a517e8ac6fabf26d50b1306207d75e307b
Sterling-Miller <stermiller11@gmail.com>
2025-03-10 14:22:03 -0700

    Cleaned code up and changed from next-auth: 5.0.0-beta.4 to 5.0.0-beta.25

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,34 +1,40 @@
 import React from "react";
 import { signOut } from "./auth";
 
-interface NavbarProps {
-    currentUser: string;
-}
+type NavbarProps = {
+  currentUser: string;
+  onSignOut: <R extends boolean = true>( options?: | { redirectTo?: string | undefined; redirect?: R | undefined } | undefined ) => Promise<R extends false ? any : never>;
+};
 
-export default function Navbar({ currentUser}: NavbarProps) {
-    return (
-        <nav className="flex items-center justify-between px-4 py-2 shadow">
-            <h1 className="text-lg font-semibold px-10">You are logged in as: {currentUser}</h1>
-            <div className="flex items-center p-5">
-                <SignOut />
-            </div>
-        </nav>
-    );
+export default function Navbar({ currentUser }: NavbarProps) {
+  return (
+    <nav className="flex items-center justify-between px-4 py-2 shadow">
+      <h1 className="text-lg font-semibold px-10">
+        You are logged in as: {currentUser}
+      </h1>
+      <div className="flex items-center p-5">
+        <SignOut />
+      </div>
+    </nav>
+  );
 }
 
 function SignOut() {
-    return (
-      <form
-        action={async () => {
-          'use server';
-          await signOut();
-        }}
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signOut();
+      }}
+    >
+      <button
+        type="submit"
+        className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800"
       >
-        <button type = "submit" className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
-<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
-Sign out
-</span>
-</button>
-      </form>
-    );
-  }
+        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+          Sign out
+        </span>
+      </button>
+    </form>
+  );
+}
--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -4,12 +4,12 @@ import Navbar from '../navbar';
 
 export default async function ProtectedPage() {
   let session = await auth();
-  let currentUser = session?.user?.email;
+  let currentUser = session?.user?.email ?? 'Guest';
 
   return (
     <div className="flex h-screen w-screen bg-slate-900 ">
       <div className="flex flex-col space-y-5 justify-center items-center text-white w-full h-full">
-        <Navbar currentUser={currentUser} onSignOut={signOut} />
+        <Navbar currentUser={currentUser ?? ''} onSignOut={signOut} />
         <QRScanner/>
       </div>
     </div>
